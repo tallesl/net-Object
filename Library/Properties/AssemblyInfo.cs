d5ef204fcfd687548e53c445ba71fca8d808eb5d
Talles L <talleslasmar@gmail.com>
2015-08-11 11:45:02 -0300

    1.1.0

--- Library/Properties/AssemblyInfo.cs
+++ Library/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DataTableToObject")]
-[assembly: AssemblyDescription("Automagically parses (with reflection) a DataTable to custom class of yours")]
+[assembly: AssemblyDescription("Automagically parses (with reflection) a DataTable to custom class of yours.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
